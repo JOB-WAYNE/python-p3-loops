2a9577fcfd3ba6cdc7ff146d4f7009beb64c23af
jobwayne <otienojobwayneotieno@gmail.com>
2024-12-01 13:00:16 +0300

    baas

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+     print (counter)
+     counter -= 1
+    print ("Happy New Year!")
 
+    # code goes here!
 def square_integers(int_list):
-    # code goes here!
-    pass
+     return [x ** 2 for x in int_list]
 
+    
 def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+      
+
     # code goes here!
-    pass
+    
